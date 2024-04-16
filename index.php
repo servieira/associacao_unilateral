c905eedcbae751f8bfb35543c506339b535d8eab
servieira <sergio.vieira@fatec.sp.gov.br>
2024-04-16 09:02:38 -0300

    linkei uma imagem no html do php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<img src="associações-uml.jpg" />
+
+
+
 <?php
     require_once "Produto.class.php";
     require_once "Fornecedor.class.php";
@@ -23,3 +37,5 @@
     }
 
 ?>
+</body>
+</html>
